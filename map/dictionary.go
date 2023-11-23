e526eb92bc98d929ff1eb7468055658c166b8d70
pymk <payamase@gmail.com>
2023-11-23 09:49:51 -0800

    init commit

---
+++ map/dictionary.go
@@ -0,0 +1,37 @@
+package dictionary
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word")
+	ErrWordExists = DictionaryErr("cannot add word if it already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Search method for Dictionary type
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
